b6169733d1052a939074a687e599d2f09c64cb41
SmallGenius <100138999+6smallgenius6@users.noreply.github.com>
2025-12-20 11:54:59 +0100

    Create decode.js

---
+++ decode.js
@@ -0,0 +1,14 @@
+// [ DECODE.JS - Grab loomian paste and decode it]
+
+//const
+const params = new URLSearchParams(window.location.search);
+const dtCode = params.get("dt");
+
+//check logic
+if (dtCode) {
+
+} else {
+
+}
+
+//methods
